851a963b7d9fff8ef1fb1f2aedcd9310fe157c84
Myles Moylan <moylanm@fastmail.com>
2023-11-12 11:33:37 -0800

    Tie SnippetModel into main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"snippetbox.mylesmoylan.net/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	logger *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -30,6 +33,7 @@ func main() {
 
 	app := &application{
 		logger: logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", "addr", *addr)
