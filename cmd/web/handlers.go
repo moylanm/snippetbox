de0abe459049567e026a2fe42032dc145fdee7a0
Myles Moylan <moylanm@fastmail.com>
2023-11-15 11:21:46 -0800

    Use validator in creation form handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,6 +9,7 @@ import (
 	"unicode/utf8"
 
 	"snippetbox.mylesmoylan.net/internal/models"
+	"snippetbox.mylesmoylan.net/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -65,7 +66,7 @@ type snippetCreateForm struct {
 	Title		string
 	Content		string
 	Expires		int
-	FieldErrors map[string]string
+	validator.Validator
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -85,24 +86,14 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		Title:		 r.PostForm.Get("title"),
 		Content:	 r.PostForm.Get("content"),
 		Expires:	 expires,
-		FieldErrors: map[string]string{},
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "this field must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
