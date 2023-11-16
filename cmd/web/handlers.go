e33a88604eb0e1822076b0a078c3162bde5bdbdd
Myles Moylan <moylanm@fastmail.com>
2023-11-16 13:21:14 -0800

    Fix typo

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -218,7 +218,7 @@ func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.sessionManager.Remove(r.Context(), "authenticateUseID")
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
 
 	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
 
