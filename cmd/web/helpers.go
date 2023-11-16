35ba35e7902b7e9684fd6a2a3507a38e28340058
Myles Moylan <moylanm@fastmail.com>
2023-11-16 13:33:29 -0800

    Update newTemplateData helper to populate IsAuthenticated field

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -30,8 +30,9 @@ func (app *application) notFound(w http.ResponseWriter) {
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:	 time.Now().Year(),
+		Flash:       	 app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
